3b129316bb8fe3403b415c95749467ac4dffd841
vshibukawa <viviane.shibukawa@gmail.com>
2019-04-04 15:12:08 +0000

    forEach

--- week_01/day_4/callback-arg.js
+++ week_01/day_4/callback-arg.js
@@ -1,10 +1,15 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
+  arr.forEach(function(value, i){
+    if( value === "Waldo") {
       found(i);   // execute callback
     }
-  }
+  });
+  // for (var i = 0; i < arr.length; i++) {
+  //   if (arr[i] === "Waldo") {
+  //     found(i);   // execute callback
+  //   }
+  // }
 }
 
 function actionWhenFound(index) {
