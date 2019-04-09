47c1acbcde2d9ed37d0e5b94b240bcbbabc58980
vshibukawa <viviane.shibukawa@gmail.com>
2019-04-09 14:56:26 +0000

    new

--- week_02/day_2/1st-web-page.js
+++ week_02/day_2/1st-web-page.js
@@ -3,7 +3,15 @@ const PORT = 8080;
 
 // a function which handles requests and sends response
 function requestHandler(request, response) {
-  response.end(`Requested Path: ${request.url}\nRequest Method: ${request.method}`);
+  if (request.url == "/") {
+    response.end("Welcome!");
+  } else if (request.url == "/urls") {
+    response.end("www.cnn.com\nwww.google.com");
+  } else {
+    response.statusCode = 404;
+    response.end("Unknown Path");
+  }
+  // response.end(`Requested Path: ${request.url}\nRequest Method: ${request.method}`);
 }
 
 var server = http.createServer(requestHandler);
