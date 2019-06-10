e80791021b4be3abaa14e3b2f2353fbc3ac13dfd
vshibukawa <viviane.shibukawa@gmail.com>
2019-06-10 11:56:27 -0400

    add challenge

--- week_07/day_4/01-tech-challenge.js
+++ week_07/day_4/01-tech-challenge.js
@@ -1,15 +1,34 @@
+// Given a string, return the character that appears the most # of times in the string.
+// If there is a tie, return the character that occurs first.
+// i.e. ‘aaaabbbbcd’, return ‘a’
+
 const most = (input) => {
 
   const inputLetters = input.split('');
-  const most = {};
-  const letters = [];
+  let most = '';
+  const letters = {};
 
   inputLetters.forEach(letter => {
 
+    // if most is empty, insert the letter
+    if(!most){
+      most = letter;
+    }
 
+    if(letters.hasOwnProperty(letter)){
+      letters[letter] += 1;
+    }else{
+      letters[letter] = 1;
+    }
+
+    if(most !== letter && letters[most] < letters[letter]){
+      most = letter;
+    }
 
   })
 
+  return most;
 }
 
-console.log(most('banana'));
+console.log('banana: ', most('banana'));
+console.log('aaaabbbbcd: ', most('aaaabbbbcd'));
